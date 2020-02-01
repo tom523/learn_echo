54437bf0c0668df05309c5467363aad9dbc10e50
tom523 <358777330@qq.com>
2020-02-01 20:39:20 +0800

    init

--- main.go
+++ main.go
@@ -1,18 +1,18 @@
 package learn_echo
 
 import (
-   "fmt" 
-   "errors"
+	"errors"
+	"fmt"
 )
 
 // Hi returns a friendly greeting
 func Hi(name string, lang string) (string, error) {
-   switch lang {
-     case "en":
-       return fmt.Sprintf("Hi, %s!", name), nil
-     case "cn":
-       return fmt.Sprintf("NiHao, %s!", name), nil
-     default:
-       return "", errors.New("unknown language")
-   }
+	switch lang {
+	case "en":
+		return fmt.Sprintf("Hi, %s!", name), nil
+	case "cn":
+		return fmt.Sprintf("NiHao, %s!", name), nil
+	default:
+		return "", errors.New("unknown language")
+	}
 }
